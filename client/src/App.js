180987394b25a688bb98b26054ab9e095e162705
Jaden Padua <paduajaden@gmail.com>
2020-04-28 13:30:18 -0700

    Create typing area

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Code Race owo </h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return(
+      <div className="container mt-5 mb-5">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">
+            <textarea
+              className="form-control mb-3"
+              placeholder="Start Typing..."
+            ></textarea>
+            <div className="text-right">
+              <button className="btn btn-light">Restart</button>
+            </div>
+          </div>
+        </div> 
+      </div>
+    );
+  }
 }
 
 export default App;
