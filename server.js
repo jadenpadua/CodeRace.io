5324eabd0947fb26964a011c24b739b1fdc330c4
Jaden Padua <paduajaden@gmail.com>
2020-05-13 18:00:28 -0700

    redirect edit

--- server.js
+++ server.js
@@ -11,14 +11,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
 
-  app.use (function (req, res, next) {
-    if (req.secure) {
-            // request was via https, so do no special handling
-            next();
-    } else {
-            // request was via http, so redirect to https
-            res.redirect('https://' + req.headers.host + req.url);
-    }
+  app.enable('trust proxy');
+  app.use(function(req, res, next) {
+      if (req.secure){
+          return next();
+      }
+      res.redirect("https://" + req.headers.host + req.url);
   });
 
     // Serve any static files
