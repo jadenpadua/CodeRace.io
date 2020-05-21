47293091544aa54a98400556f6fbe7dcf4ede1c7
Jaden Padua <paduajaden@gmail.com>
2020-05-21 15:25:36 -0700

    Suggestion Page

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -6,10 +6,10 @@ import './styles.css'
 const NavBar = () => {
     return (
         <nav className = "nav-wrapper container-fluid">
-            <div class="row h-100">
-                <div class="col-2 home h-100"> <img class = "img-fluid w-99 px-0" src = {home} alt = "home"></img> </div>
-                <div class="col-8 logo mt-4 text-responsive"><h1>CodeRace.io</h1></div>
-                <div class="col-2 hamburger"><img class = "img-fluid w-99 px-0" src = {hamburger} alt = "home" ></img></div>
+            <div className="row h-100">
+                <div className="col-2 home h-100"> <img className = "img-fluid w-99 px-0" src = {home} alt = "home"></img> </div>
+                <div className="col-8 logo mt-4 text-responsive"><h1>CodeRace.io</h1></div>
+                <div className="col-2 hamburger"><img className = "img-fluid w-99 px-0" src = {hamburger} alt = "home" ></img></div>
             </div>
         </nav>
     )
--- client/src/components/Suggestion/Suggestion.js
+++ client/src/components/Suggestion/Suggestion.js
@@ -1,11 +1,61 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import TextareaAutosize from 'react-autosize-textarea';
+import Navbar from '.././Navbar/Navbar.js';
+import './styles.css'
 const Suggestion = (props) => {
+const [userInput,setUserInput] = useState("")
 
+   const submit = () => {
+      setUserInput("")
+    }
+
+
+      
+   
+    const onUserInputChange = (e) => {
+      const v = e.target.value;
+      setUserInput(v)
+      console.log(userInput)
+
+    }
 
     return (
+      <div>
+      <Navbar></Navbar>
+      <div className="text-center">
+         <div className="suggestion-text">
+            <h2>Suggestion Box</h2>
+         </div>
+      </div>
 
-       <div>Suggestion</div>
+        <div className="container mt-4 mb-5">
+        
+          
+               <div className="mx-auto suggestion border rounded p-3 mb-4 mh-100 ">
+                  
+             
+                  <div className = "text-center">
+                     <div className = "box-text">Have a suggestion? Any comments? Any bugs you ran into while playing CodeRace?</div>
+                  </div>
+               </div>
+             
+
+               <TextareaAutosize
+               className="mx-auto suggestion form-control p-3 mb-3"
+               placeholder="It would be really cool if you could add..." 
+               value={userInput} 
+               onChange={onUserInputChange}
+               >
+                   
+               </TextareaAutosize> 
+            
+              
+            
+               <div className="suggestion-button"> <button className="btn btn-light" onClick = {submit} >Submit</button></div>
+            </div>
+         
+          </div> 
+      
     );
   }
 
